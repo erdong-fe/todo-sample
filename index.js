070384c23da333f9965c02394136f9e46cf24073
erdong <zhiguangphoenix@gmail.com>
2022-12-10 20:53:46 +0800

    feat: 编辑todo和完成todo

--- index.js
+++ index.js
@@ -51,11 +51,23 @@ const APIs = (() => {
     const getTodos = () => {
         return fetch(URL).then((res) => res.json());
     };
+
+    const updateTodo = (id, newTodo) => {
+        return fetch(
+            URL + `/${id}`,
+            {
+                method: 'PUT',
+                body: JSON.stringify(newTodo),
+                headers: { "Content-Type": "application/json" },
+            }
+        ).then((res) => res.json());
+    }
     
     return {
         addTodo,
         removeTodo,
         getTodos,
+        updateTodo,
     };
 })();
 
@@ -63,7 +75,9 @@ const Model = (() => {
     //todolist
     class State {
         #todos; //[{id: ,title: },{}]
+        #editId=0;    // 当前正在被编辑的todoId
         #onChange;
+        editContent=""; // 当前被编辑的内容
         constructor() {
             this.#todos = [];
         }
@@ -81,17 +95,27 @@ const Model = (() => {
             this.#onChange?.();
         }
 
+        get editId() {
+            return this.#editId;
+        }
+
+        set editId(newEditId) {
+            this.#editId = parseInt(newEditId);
+            this.#onChange();
+        }
+
         subscribe(callback) {
             this.#onChange = callback;
         }
     }
-    let { getTodos, removeTodo, addTodo } = APIs;
+    let { getTodos, removeTodo, addTodo, updateTodo } = APIs;
 
     return {
         State,
         getTodos,
         removeTodo,
         addTodo,
+        updateTodo
     };
 })();
 //BEM, block element modifier methodology
@@ -99,9 +123,24 @@ const View = (() => {
     const formEl = document.querySelector(".form"); //querying
     const todoListEl = document.querySelector(".todo-list");
     const updateTodoList = (todos) => {
+        console.log('updateTodoList');
         let template = "";
         todos.forEach((todo) => {
-            const todoTemplate = `<li><span>${todo.title}</span><button class="btn--delete" id="${todo.id}">remove</button></li>`;
+            const todoContentToShow = `<span class="${todo.completed ? 'todo-content-completed todo-content' : 'todo-content'}" id="${todo.id}" todo='${JSON.stringify(todo)}'>${todo.title}</span>`;
+            const todoContentToEdit = `<input class="input-edit" value="${todo.title}" />`
+            const isEditingCurTodo = todo.id === ViewModel.state.editId;
+
+            const todoTemplate = `<li>
+                    ${isEditingCurTodo ? todoContentToEdit : todoContentToShow}
+                    <div class="btn-box">
+                        <button class="btn-edit" id="${todo.id}" todo='${JSON.stringify(todo)}'>
+                            ${isEditingCurTodo ? 'ok' : 'edit'}
+                        </button>
+                        <button class="btn-delete" id="${todo.id}">
+                            remove
+                        </button>
+                    </div>
+                </li>`;
             template += todoTemplate;
         });
         if(todos.length === 0){
@@ -151,10 +190,11 @@ const ViewModel = ((View, Model) => {
                 return;
             }
             console.log("title", title);
-            const newTodo = { title };
+            const newTodo = { title, completed: false };
             Model.addTodo(newTodo)
                 .then((res) => {
-                    state.todos = [res, ...state.todos];
+                    // state.todos = [res, ...state.todos];
+                    getTodos();
                     event.target[0].value = ""
                 })
                 .catch((err) => {
@@ -169,7 +209,7 @@ const ViewModel = ((View, Model) => {
             //console.log(event.target/* emit the event */, event.currentTarget/* receive the event */);
             const id = event.target.id;
             //console.log("id", id)
-            if(event.target.className === "btn--delete"){
+            if(event.target.className === "btn-delete"){
                 Model.removeTodo(id).then(res=>{
                     state.todos = state.todos.filter(todo=> +todo.id !== +id)
                 }).catch(err=>alert(`delete todo failed: ${err}`))
@@ -177,10 +217,60 @@ const ViewModel = ((View, Model) => {
         })
     };
 
+    const editTodo = () => {
+        View.todoListEl.addEventListener(
+            "click",
+            (event) => {
+                const id = event.target.id;
+                const todo = JSON.parse(event.target.getAttribute('todo'));
+
+                if (event.target.className === 'btn-edit') {
+                    if (state.editId === 0) {
+                        state.editId = id;
+                    } else {
+                        if (state.editId === parseInt(id)) {
+                            const editInput = document.getElementsByClassName('input-edit')[0];
+                            Model.updateTodo(id, { ...todo, title: editInput.value })
+                            .then(res=>{
+                                getTodos();
+                                state.editId = 0;
+                            })
+                            .catch(err=>alert(`update todo failed: ${err}`))
+                        } else {
+                            state.editId = id;
+                        }
+                    }
+                }
+            }
+
+        )
+    }
+
+    const completeTodo = () => {
+        View.todoListEl.addEventListener(
+            'click',
+            (event) => {
+                const id = event.target.id;
+                const todo = JSON.parse(event.target.getAttribute('todo'));
+                console.log(todo);
+                if (event.target.classList.contains('todo-content')) {
+                    Model.updateTodo(id, { ...todo, completed: !todo.completed })
+                    .then(res=>{
+                        getTodos();
+                        state.editId = 0;
+                    })
+                    .catch(err=>alert(`complete todo failed: ${err}`))
+                }
+            }
+        )
+    }
+
     const bootstrap = () => {
         addTodo();
         getTodos();
         removeTodo();
+        editTodo();
+        completeTodo();
         state.subscribe(() => {
             View.updateTodoList(state.todos);
         });
@@ -188,6 +278,7 @@ const ViewModel = ((View, Model) => {
 
     return {
         bootstrap,
+        state,
     };
 })(View, Model);
 
